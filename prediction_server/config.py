ad9a06ef658b48401a5f5ef0c31e541b27e51db0
Eyal Perry <eyal.perry88@gmail.com>
2018-11-02 16:35:14 +0200

    Changed server flow to allow continuous upload

--- prediction_server/config.py
+++ prediction_server/config.py
@@ -1,9 +1,11 @@
-# amount of data points tthe training data must have
+# number of data points the data must have in order to train
 min_train_data_size = 600
+# number of data points to use for prediction
+prediction_data_size = 600
 
-# where to locally store the training data
+# where to locally store the data
 # all backups will have the same path, with an added suffix
-train_data_filename = "data/train_data.csv"
+data_filename = "data/data.csv"
 # where to locally store the trained model
 model_filename = "model.pkl"
 
